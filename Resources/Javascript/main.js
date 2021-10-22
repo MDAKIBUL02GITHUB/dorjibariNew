ec162be4fc68bd65bd1fd06cf372b37e9796e8eb
dorjibari <mdakibulislam02@gmail.com>
2021-10-23 00:30:32 +0600

    dorjibari

---
+++ Resources/Javascript/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+    //STICKY MENU
+    $(".js--services-section").waypoint(function(direction){
+        if(direction == "down"){
+           $("nav").addClass("sticky");
+        }else{
+           $("nav").removeClass("sticky");
+        }
+    });
+});
